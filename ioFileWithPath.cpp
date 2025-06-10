0276481d339409b109ddf4d106f87c09945cf2e2
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-06-10 15:26:03 +0700

    membuat membuka file dalam mode menulis dan membuat outfile untuk menunjuk ke sebuah nama file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -9,4 +9,10 @@ int main() {
 
     cout << "Masukkan Nama File : " << endl;
     cin >> NamaFile;
+
+    // membuka file dalam mode menulis
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
 }
