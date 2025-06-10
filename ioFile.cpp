3cca0d83f99668c6a6dc679f9b084b29a201fce4
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-06-10 14:55:16 +0700

    membuat ifstream dan membuat infile untuk menunjuk ke sebuah file

--- ioFile.cpp
+++ ioFile.cpp
@@ -25,4 +25,9 @@ int main() {
     }
     // selesai dalam menulis sekarang tutup filenya
     outfile.close();
+    // membuka file dalam mode baca
+    ifstream infile;
+    // menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+    cout << endl << ">= Membuka dan membaca file " << endl;
 }
