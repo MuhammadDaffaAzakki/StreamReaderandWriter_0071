ff1557ef1f4463b337fb7ee15e934742645958f7
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-06-10 15:29:12 +0700

    membuat while (true) dan if

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -15,4 +15,14 @@ int main() {
     // menunjuk ke sebuah nama file
     outfile.open(NamaFile + ".txt", ios::out);
     cout << ">= Menulis File, \'q\' untuk keluar" << endl;
+    // unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
 }
